a3e2958202c5f7b5cf9f3536a7b2e2187a1df23c
Charles Johnson <charlesthomasjohnson0@gmail.com>
2025-08-29 14:48:47 +0900

    Update as_expression.rs

--- diesel_derives/src/as_expression.rs
+++ diesel_derives/src/as_expression.rs
@@ -5,7 +5,6 @@ use meta::*;
 use util::*;
 
 pub fn derive(item: syn::DeriveInput) -> Result<proc_macro2::TokenStream, Diagnostic> {
-    let dummy_mod = format!("_impl_as_expression_for_{}", item.ident,).to_lowercase();
     let flags =
         MetaItem::with_name(&item.attrs, "diesel").unwrap_or_else(|| MetaItem::empty("diesel"));
     let is_sized = !flags.has_flag("not_sized");
@@ -102,17 +101,9 @@ pub fn derive(item: syn::DeriveInput) -> Result<proc_macro2::TokenStream, Diagno
     });
 
     if any_sql_types {
-        Ok(wrap_in_dummy_mod(
-            Ident::new(&dummy_mod, Span::call_site()),
-            quote! {
-                use diesel::expression::AsExpression;
-                use diesel::expression::bound::Bound;
-                use diesel::sql_types::Nullable;
-                use diesel::serialize::{self, ToSql, Output};
-
-                #(#tokens)*
-            },
-        ))
+        Ok(quote! {
+            #(#tokens)*
+        })
     } else {
         Ok(quote!())
     }
